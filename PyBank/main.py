17dca61d3efa8a764f170c766fec16f31d7ef39c
BradleyHigdon <bhigdon1@live.maryville.edu>
2020-08-20 13:50:27 -0500

    Beginning initial code

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,3 +5,26 @@ budget_data = os.path.join("Pybank", "Resources", "budget_data.csv")
 
 with open(budget_data) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
+
+
+    print("Financial Analysis")
+    print("----------------------------")
+    #Counting the months
+    #print(sum["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+
+    #Total profit/loss
+    #for row in csv.reader:
+    #print(sum[])
+
+    #Average profit/loss
+    #print
+
+    #Greatest increase in profits
+    #for row in csvreader:
+    #if
+    #print(Greatest Increase in Profits:)
+
+    #Greatest decrease in profits
+    #for row in csvreader:
+    #if
+    #print(Greatest Decrease in Profits:)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,3 +5,23 @@ election_data = os.path.join("PyPoll", "Resources", "election_data.csv")
 
 with open(election_data) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
+
+
+    print("Election Results")
+    print("-------------------------")
+    #Total votes cast
+    #print(sum (total row - 1))
+
+
+    print("-------------------------")
+    #Candidates that recieved votes
+    #print
+
+    #Total votes for each candidate
+    #print
+
+    print("-------------------------")
+    #Winning candidate
+    #print(Candidate_Name)
+
+    print("-------------------------")
