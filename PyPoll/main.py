d288f9183813d37deba19c789553e826c649cc8c
BradleyHigdon <bhigdon1@live.maryville.edu>
2020-08-22 20:36:06 -0500

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,6 +12,12 @@ with open(election_data) as csvfile:
     print("Election Results")
     print("-------------------------")
 
+    #Creating variables for votes per candidate
+    Khan_votes = 0
+    Correy_votes = 0
+    Li_votes = 0
+    OTooley_votes = 0
+
     #Creating lists for Voter ID, Candidates, and votes cast
     voterID = []
     county = []
@@ -32,11 +38,26 @@ with open(election_data) as csvfile:
     #Candidates that recieved votes
     candidate_set = set(candidate)
     number_of_unique_values = len(candidate_set)
-    print(candidate_set)
+    
 
     #Total votes for each candidate
-    #print
+    if (row[2] == "Khan"): Khan_votes += 1
+    if (row[2] == "Correy"): Correy_votes += 1
+    if (row[2] == "Li"): Li_votes += 1
+    if (row[2] == "O'Tooley"): OTooley_votes += 1
 
+    #Percentage of votes for each candidate
+    Kahn_votes_percentage = ((Khan_votes / total_votes)*100)
+    Correy_votes_percentage = ((Correy_votes / total_votes)*100)
+    Li_votes_percentage = ((Li_votes / total_votes)*100)
+    OTooley_votes_percentage = ((OTooley_votes / total_votes)*100)
+
+    #Print statements for candidates who received votes, percentages of the votes, and vote totals for each candidate
+    print("Candidates who received votes",candidate_set)
+    print("Khan vote total:",Kahn_votes_percentage,"%",Khan_votes)
+    print("Correy vote total:",Correy_votes_percentage,"%",Correy_votes)
+    print("Li vote total:",Li_votes_percentage,"%",Li_votes)
+    print("O'Tooley vote total:",OTooley_votes_percentage,"%",OTooley_votes)
     print("-------------------------")
     #Winning candidate
     #print(Candidate_Name)
